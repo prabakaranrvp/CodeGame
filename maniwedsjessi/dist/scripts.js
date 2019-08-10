990fb8771cea226335ff203c2e43ec7a56bf12cf
Prabakaran Raja <prabakaranrvp@gmail.com>
2019-08-10 18:27:20 +0530

    Mani Wedding changes - req by Mr. Yamaha Mani

--- maniwedsjessi/dist/scripts.js
+++ maniwedsjessi/dist/scripts.js
@@ -856,7 +856,7 @@ $(document).ready(function() {
 	};
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Jul 8, 2018 09:00:00").getTime();
+		var countDownDate = new Date("Aug 30, 2019 18:00:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
