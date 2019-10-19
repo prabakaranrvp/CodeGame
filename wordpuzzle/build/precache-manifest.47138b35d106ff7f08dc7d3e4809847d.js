5c24a7d770d3565c99ac2e4975c910d7e2cdfd01
Prabakaran Raja <prabakaranrvp@gmail.com>
2019-10-20 02:08:16 +0530

    Commiting Build Files

---
+++ wordpuzzle/build/precache-manifest.47138b35d106ff7f08dc7d3e4809847d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ef0ef01d91f2f17740e2825ca3d607",
+    "url": "/wordpuzzle/index.html"
+  },
+  {
+    "revision": "6bae1131c98489b872e7",
+    "url": "/wordpuzzle/static/css/main.994ceb24.chunk.css"
+  },
+  {
+    "revision": "6ee7e445129de1e79f28",
+    "url": "/wordpuzzle/static/js/0.1d018da1.chunk.js"
+  },
+  {
+    "revision": "204ef012ef7555621f9a",
+    "url": "/wordpuzzle/static/js/1.e69894d2.chunk.js"
+  },
+  {
+    "revision": "12eda2eb3a8b277bcdb3",
+    "url": "/wordpuzzle/static/js/4.4026d0f1.chunk.js"
+  },
+  {
+    "revision": "6bae1131c98489b872e7",
+    "url": "/wordpuzzle/static/js/main.f46f38ff.chunk.js"
+  },
+  {
+    "revision": "3b3756a4a0088fdb8f42",
+    "url": "/wordpuzzle/static/js/runtime~main.23fa0d2f.js"
+  }
+]);
